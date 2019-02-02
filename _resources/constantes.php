1a95191c5d905042801fd97d42396c9d564fee9a
arthur <arthur@192.168.0.7>
2019-02-02 18:38:29 +0100

    conflit constantes

--- _resources/constantes.php
+++ _resources/constantes.php
@@ -19,7 +19,8 @@ define('DATE_HTML_T',"Y-m-d\TH:i");
 define('DATE_HTML_T_SEC',"Y-m-d\TH:i:s");
 
 //define('ROOT',"/EclipsePhpWorkSpace/deadornot/"); //Antoine boulot
-define('ROOT',"/WorkspaceEclipsePhp/deadornot/"); //Antoine maison
+//define('ROOT',"/WorkspaceEclipsePhp/deadornot/"); //Antoine maison
+define('ROOT',"/deadornot/"); //Arthur
 //define('ROOT',"/"); //production
 //define('PAGE_SITE_ROOT', "https://shotgun.ecam.fr/");
 define('PAGE_MENU',ROOT."menu/menu.php");
