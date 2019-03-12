23a26c9c7485b9783f41d7ff4c75e8c196ef766e
antoine.gagneux <antoine.gagneux@ecam.fr>
2019-03-12 17:28:42 +0100

    debug autocomplete css

--- menu/menu_list_ajax.php
+++ menu/menu_list_ajax.php
@@ -11,16 +11,17 @@ if (!isset($_POST['query']) || empty($_POST['query'])) {
 
 $result='{"query": "Unit","suggestions": [';
 
-if (strlen($requestTxt)>NBCARACTCHERCHE) {
+
     $tableauResult=chercherListeFromTxt($requestTxt);
     if ($tableauResult==null) $tableauResult=array();
-} else { 
-    $tableauResult=array();
-}
+
 
 foreach ($tableauResult as $ligne){
     $result.='{ "value" : "'.$ligne['title'].'", "data" : "'.$ligne['pageid'] .'" },';
 }
+
+//$result.='{ "value" : "toto", "data" : "22" },';
+//$result.='{ "value" : "ggre", "data" : "56" },';
 $result = substr($result,0,strlen($result)-1);
 $result.=']}';
 echo $result;
